c6f76f9590fa6d82f5e0dffe1cde4b1630cb3d80
okaki1999 <140598093+okaki1999@users.noreply.github.com>
2024-07-01 07:23:57 +0900

    deviceの導入

--- evev_api/config/initializers/cors.rb
+++ evev_api/config/initializers/cors.rb
@@ -17,11 +17,12 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'localhost:3001' ,"https://web.fuji-blog.site"# Next.jsを動作させているアドレスとポート番号
+      origins 'localhost:3000' ,"https://web.fuji-blog.site"# Next.jsを動作させているアドレスとポート番号
       
   
       resource '*',
         headers: :any,
+        expose: ["access-token", "expiry", "token-type", "uid", "client"],
         methods: [:get, :post, :put, :patch, :delete, :options, :head]
     end
 end
